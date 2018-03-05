9e68c45bab98e24c0b3f1b50e094d6ec358ae65a
elliedlouha <ellie.dlouha@hotmail.com>
2018-03-04 22:09:15 -0500

    two diff data

--- week 5/sketch.js
+++ week 5/sketch.js
@@ -11,29 +11,49 @@ function parseJson(data) {
 }
 
 function draw() {
-  background(255,255,255);
+  background(0);
 
   for (var i = 0; i < positions.length; i++) {
   	positions[i].display();
   }
+  fill(255);
+  textSize(30);
+  text("Welcome to 2D drawing", 40, 50);
+  fill(100,20,90);
+  textSize(30);
+  text("Click the key R to draw in red circles", 40, 100);
+  fill(90,20,120);
+  textSize(30);
+  text("Click the key B to draw in blue squares", 40, 150);
 }
 
 function PaintMark(pos) {
 	this.position = pos;
 
 	this.display = function() {
+		if (key === 'B'){
 		noStroke();
-		fill(255, 0, 255);
-		ellipse(this.position.x, this.position.y, 10, 10);
+		fill(0, 0, 255);
+		rect(this.position.x, this.position.y, 20, 20);
+	}if (key === 'R'){
+		noStroke();
+		fill(255, 0, 0);
+		ellipse(this.position.x, this.position.y, 20, 20);
+	}
 	}
 }
 
 function keyPressed() {
 	if (key === ' ') {
-		savePaint();
+		//savePaint();
+		this.display = function() {
+		noStroke();
+		fill(255, 0, 0);
+		ellipse(this.position.x, this.position.y, 60, 60);
+	}
 	}
 	if (key === 'L') {
-		loadJSON('paint.json', parseJson);
+		//loadJSON('paint.json', parseJson);
 	}
 }
 
