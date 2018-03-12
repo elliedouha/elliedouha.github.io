e3d6cb646218da89d0b20da000b8a14ea0ea874f
elliedlouha <ellie.dlouha@hotmail.com>
2018-03-12 14:08:19 -0400

    update 3

--- week6/sketch.js
+++ week6/sketch.js
@@ -12,58 +12,58 @@
 // 	ellipse(mouseX, mouseY, 100, 100);
 // }
 
-var sceneData;
+// var sceneData;
 
-var currentScene = 0;
-var scenes = [];
+// var currentScene = 0;
+// var scenes = [];
 
-function preload() {
-  sceneData = loadJSON('scenes.json');
-}
+// function preload() {
+//   sceneData = loadJSON('scenes.json');
+// }
 
-function setup() {
-  createCanvas(800, 800);
-  CreateScenesFromData(sceneData.scenes);
-}
+// function setup() {
+//   createCanvas(800, 800);
+//   CreateScenesFromData(sceneData.scenes);
+// }
 
-function draw() {
-  background(255);
-  scenes[currentScene].display();
+// function draw() {
+//   background(255);
+//   scenes[currentScene].display();
 
-  fill(0);
-  textSize(24);
-  text("press the option number to advance to the indicated scene", 50, 700);
-}
+//   fill(0);
+//   textSize(24);
+//   text("press the option number to advance to the indicated scene", 50, 700);
+// }
 
-function CreateScenesFromData(data) {
-  for (var i = 0; i < data.length; i++) {
-    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes))
-  }
-}
+// function CreateScenesFromData(data) {
+//   for (var i = 0; i < data.length; i++) {
+//     scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes))
+//   }
+// }
 
-function Scene(sceneText, options, nextScenes) {
-  this.sceneText = sceneText;
-  this.options = options;
-  this.nextScenes = nextScenes;
+// function Scene(sceneText, options, nextScenes) {
+//   this.sceneText = sceneText;
+//   this.options = options;
+//   this.nextScenes = nextScenes;
 
-  this.display = function() {
-    fill(0);
-    textSize(32);
-    text(this.sceneText, 100, 100);
+//   this.display = function() {
+//     fill(0);
+//     textSize(32);
+//     text(this.sceneText, 100, 100);
 
-    for (var i = 0; i < options.length; i++) {
-      text('OPTION ' + (i + 1) + ': ' + this.options[i], 150, 200 + i * 50);
-    }
-  }
-}
+//     for (var i = 0; i < options.length; i++) {
+//       text('OPTION ' + (i + 1) + ': ' + this.options[i], 150, 200 + i * 50);
+//     }
+//   }
+// }
 
-function keyPressed() {
-  var numberPressed = parseInt(key);
-  var newScene = scenes[currentScene].nextScenes[numberPressed - 1];
-  if (newScene !== undefined) {
-    currentScene = newScene;
-  }
-}
+// function keyPressed() {
+//   var numberPressed = parseInt(key);
+//   var newScene = scenes[currentScene].nextScenes[numberPressed - 1];
+//   if (newScene !== undefined) {
+//     currentScene = newScene;
+//   }
+// }
 
 ////////////////////////////////////////////////////////////////
 
