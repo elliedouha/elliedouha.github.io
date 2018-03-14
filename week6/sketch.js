796be8640da8a547c8e885d243d205007b90f9d9
elliedlouha <ellie.dlouha@hotmail.com>
2018-03-14 13:48:15 -0400

    it works

--- week6/sketch.js
+++ week6/sketch.js
@@ -1,58 +1,75 @@
-// function setup() {
-// 	createCanvas(1300, 800);
-// }
-
-// function preload(){
-// 	oBackground = loadImage('assets/chaoticColor.jpg');
-	
-// }
-
-// function draw()  {
-// 	fill(0);
-// 	ellipse(mouseX, mouseY, 100, 100);
-// }
-
 var sceneData;
 
 var currentScene = 0;
+var oldScene =0;
 var scenes = [];
+var r2 , g2, b2 ;
 
 function preload() {
   sceneData = loadJSON('scenes.json');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1250, 650);
   CreateScenesFromData(sceneData.scenes);
 }
 
 function draw() {
-  background(255);
-  scenes[currentScene].display();
 
+//changes the backgorund only once
+  scenes[currentScene].display();
+    if (currentScene != oldScene){
+  	// console.log("old scene is" + oldScene) ;
+  	// console.log("changing to scene" + currentScene) ;
+  	background(r2,g2,b2);
+  	oldScene = currentScene;
+}
+//curser white circle
+  fill(255);
+  noStroke();
+  ellipse(mouseX, mouseY, 80, 80);
+
+//directions
   fill(0);
+  textSize(15);
+  text("Clear away the color, become zen", 50, 570);
   textSize(24);
-  text("press the option number to advance to the indicated scene", 50, 700);
+  //text("press the option number to advance to the indicated scene", 50, 500);
+  text("How are you feeling today? What emotion do you need to clear? Pick your zen world", 200, 20);
 }
 
 function CreateScenesFromData(data) {
   for (var i = 0; i < data.length; i++) {
-    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes))
+    scenes.push(new Scene(data[i].sceneText, data[i].options, data[i].nextScenes, data[i].r, data[i].g, data[i].b))
   }
 }
 
-function Scene(sceneText, options, nextScenes) {
+function Scene(sceneText, options, nextScenes, r, g, b) {
   this.sceneText = sceneText;
   this.options = options;
   this.nextScenes = nextScenes;
+  this.r = r;
+  this.g = g;
+  this.b = b;
 
   this.display = function() {
+  	//title format
     fill(0);
-    textSize(32);
-    text(this.sceneText, 100, 100);
-
+    textSize(40);
+    //title location
+    text(this.sceneText, 450, 80);
+    //background colors
+    r2 = this.r;
+    g2 = this.g;
+    b2 = this.b;
+    console.log(r) ;
+    console.log(g) ;
+    console.log(this.b) ;
+    //zen text
+    textSize(20);
+    fill(50);
     for (var i = 0; i < options.length; i++) {
-      text('OPTION ' + (i + 1) + ': ' + this.options[i], 150, 200 + i * 50);
+      text('Zen Option ' + (i + 1) + ': ' + this.options[i], 480, 120 + i * 30);
     }
   }
 }
@@ -61,135 +78,9 @@ function keyPressed() {
   var numberPressed = parseInt(key);
   var newScene = scenes[currentScene].nextScenes[numberPressed - 1];
   if (newScene !== undefined) {
+  	oldScene = currentScene;
     currentScene = newScene;
   }
 }
 
-////////////////////////////////////////////////////////////////
-
-// var sceneState = {
-//   INTRO: 0,
-//   SAD: 1,
-//   ANGRY: 2,
-//   OVERWHELMED: 3
-// };
-
-// var currentState = sceneState.INTRO;
-
-// var oBackgroundImg;
-
-// var sceneData;
-
-// var currentScene = 0;
-// var scenes = [];
-
-
-
-
-// function setup() {
-//   createCanvas(400,600);
-
-//   oBackgroundImg = loadImage("assets/chaoticColor.png");
-
-
-// }
-
-// function draw() {
-//   drawScene(currentState);
-//   checkTransition(currentState);
-// }
-
-
-
-// //SCENES
-
-
-//   function drawScene(whichScene) {
-//   switch (currentState) {
-//     case sceneState.INTRO:
-//       background(255);
-//       textSize(20);
-//       textAlign(CENTER, CENTER);
-//       text("Find\nYour\n\"Zen\"", width/2, height/2);
-//       break;
-//     case sceneState.SAD:
-//         background(0, 0, 255);
-        
-//     case sceneState.ANGRY:
-//         background(255, 0, 0);
-           
-//     case sceneState.OVERWHELMED:
-       
-//         background(0, 255, 0);
-
-//     }
-//       break;
-//       default:
-//       break;
-
-//     }
-
-//   }
-
-
-// //TRANSITIONS
-
-
-//   function checkTransition(whichScene) {
-//   switch (whichScene) {
-//     case sceneState.INTRO:
-//       if (key == 'H') {
-//         currentState = sceneState.SAD;
-//         setUpScene(currentState);
-//       }
-//       break;
-//     case sceneState.SAD:
-//       if (key == 'S') {
-        
-//           currentState++;
-//           setUpScene(currentState);      
-        
-//       }
-//       break;
-//     case sceneState.ANGRY:
-//       if (key == 'A') {
-//          {
-//           currentState = sceneState.INTRO;      
-//         setUpScene(currentState);
-//       }
-//     }
-//       break;
-//     case sceneState.OVERWHELMED:
-//       if (key == 'O') {
-        
-//           currentState++;
-//           setUpScene(currentState);      
-        
-//       }
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-
-// function setUpScene(whichScene) {
-//   switch (whichScene) {
-//     case sceneState.INTRO:
-//       break;
-//     case sceneState.SAD:
-//       break;
-//     case sceneState.ANGRY:
-//       break;
-//       case sceneState.OVERWHELMED:
-//       break;
-//     default:
-//       break;
-//   }
-// }
-
-
-// function keyPressed() {
-//   keyOn = true;
-// }
 
